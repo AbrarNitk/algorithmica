0f57e8d73f3c21c6998207223eac700c78aa56a0
AbrarNitk <abrar.nitk@gmail.com>
2019-07-19 13:02:39 +0530

    merge sort

---
+++ src/graph/connectivity.rs
@@ -0,0 +1,32 @@
+
+/// Equivalence Graph properties
+/// 1. x Connected to itself
+/// 2. if x is connected to y means y is connected to x
+/// 3. if x is connected to y means y is connected to z means x is connected to z
+///
+/// Given connectivity between nodes
+/// connectivity ==> <--->
+/// x <---> y, x <---> a, x <---> b, t <---> y, u <---> y, r <---> g, g <---> t
+/// Need to find whether two nodes are connected or not.
+/// Like, is x connected to g
+///
+/// This problem is related to Disjoint sets
+///
+///
+pub struct UnionFind {
+    data: Vec<usize>
+}
+
+pub struct UFLazy(UnionFind);
+
+impl UFLazy {
+
+    pub fn union(&mut self, n1: usize, n2: usize) {
+
+    }
+
+    pub fn find(&self, n1: usize, n2: usize) -> bool {
+        true
+    }
+
+}
--- src/sort/merge_sort.rs
+++ src/sort/merge_sort.rs
@@ -1,1 +1,97 @@
+use std::cmp::{Ord, Ordering};
+use std::fmt::Debug;
+use std::ptr;
+use std::mem;
 
+unsafe fn get_by_index<T>(list: &[T], index: isize) -> *const T {
+    let list_offset = list.as_ptr();
+    list_offset.offset(index)
+}
+
+fn merge<T: Ord + Debug>(list: &mut [T], start: usize, mid: usize, end: usize) {
+    let mut list_l = list.as_mut_ptr();
+
+    let mut left = Vec::with_capacity(mid - start + 1);
+    let mut right = Vec::with_capacity(end - mid);
+    unsafe {
+        // list_l = list_l.offset((start * mem::size_of::<T>()) as isize);
+        // left.set_len(mid - start + 1);
+        let mut start=  start;
+        while start <= mid {
+            left.push(get_by_index(list, start as isize).read());
+            start += 1;
+        }
+        // ptr::copy_nonoverlapping(list_l, left.as_mut_ptr(), mid - start + 1);
+        // list_l = list_l.offset((mid * mem::size_of::<T>()) as isize);
+        // right.set_len(end - mid);
+        while start <= end {
+            right.push(get_by_index(list, start as isize).read());
+            start += 1;
+        }
+        // ptr::copy_nonoverlapping(list_l, right.as_mut_ptr(), end - mid);
+    }
+
+    let mut left_index = 0;
+    let mut right_index = 0;
+    let mut k = start;
+
+    unsafe {
+        while left_index < left.len() && right_index < right.len() {
+            match left[left_index].cmp(&right[right_index]) {
+                Ordering::Less | Ordering::Equal => {
+                    list[k] = get_by_index(&left, left_index as isize).read();
+                    left_index += 1;
+                }
+                Ordering::Greater => {
+                    list[k] = get_by_index(&right, right_index as isize).read();
+                    right_index += 1;
+                }
+            }
+            k += 1;
+        }
+
+        while left_index < left.len() {
+            list[k] = get_by_index(&left, left_index as isize).read();
+            left_index += 1;
+            k += 1;
+        }
+
+        while right_index < right.len() {
+            list[k] = get_by_index(&right, right_index as isize).read();
+            right_index += 1;
+            k += 1;
+        }
+    }
+}
+
+fn merge_sort<T: Ord + Debug>(list: &mut [T], start: usize, end: usize) {
+    if end <= start {
+        return;
+    }
+    let mid = (end - start) / 2 + start;
+    merge_sort(list, start, mid);
+    merge_sort(list, mid + 1, end);
+    merge(list, start, mid, end);
+}
+
+pub fn sort<T>(list: &mut [T])
+where
+    T: Ord + Debug,
+{
+    if list.is_empty() || list.len() == 1 {
+        return;
+    }
+    merge_sort(list, 0, list.len() - 1);
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    #[test]
+    fn sorting_test() {
+        let mut t = [1, 2, 3, 4, 5,6,7,8];
+        t.reverse();
+        sort(&mut t);
+        assert_eq!([1, 2, 3, 4, 5,6,7,8], t);
+    }
+}
