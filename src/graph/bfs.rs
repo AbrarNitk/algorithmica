a4f38eb81db6dfdc2881903c0bc1edaab522187f
AbrarNitk <abrar.nitk@gmail.com>
2019-09-12 23:19:21 +0530

    dfs for digraph

---
+++ src/graph/bfs.rs
@@ -0,0 +1,46 @@
+use crate::graph::Graph;
+use crate::search::binary::search;
+
+pub struct BFS<'a> {
+    source: usize,
+    visited: Vec<bool>,
+    parent: Vec<usize>,
+    dist: Vec<usize>,
+    graph: &'a Graph,
+}
+
+impl<'a> BFS<'a> {
+    pub fn new(source: usize, graph: &'a Graph) -> Self {
+        let mut new = Self {
+            source,
+            visited: vec![false; graph.v],
+            parent: vec![0; graph.v],
+            dist: vec![0; graph.v],
+            graph,
+        };
+        new.bfs(source);
+        new
+    }
+
+    pub fn has_path(&self, w: usize) -> bool {
+        self.visited[w]
+    }
+
+    fn bfs(&mut self, source: usize) {
+        let mut queue = Vec::new();
+        queue.push(source);
+        self.visited[source] = true;
+        self.dist[source] = 0;
+        self.parent[source] = 0; // TODO: Fix Me
+        while !queue.is_empty() {
+            let front = queue.first().unwrap();
+            for w in self.graph.adj(*front).iter() {
+                if !self.visited[*w] {
+                    self.visited[*w] = true;
+                    self.dist[source] = self.dist[*front] + 1;
+                    self.parent[source] = *front;
+                }
+            }
+        }
+    }
+}
--- src/graph/mod.rs
+++ src/graph/mod.rs
@@ -1,5 +1,6 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
+pub mod bfs;
 pub mod depth_first_paths;
 
 #[derive(Debug)]
