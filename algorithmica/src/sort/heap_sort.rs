fb318b645d01763096588edbab63b2df52cc659b
Abrar <abrar.nitk@gmail.com>
2021-06-13 08:50:13 +0530

    heap sort by fn

--- algorithmica/src/sort/heap_sort.rs
+++ algorithmica/src/sort/heap_sort.rs
@@ -1,8 +1,10 @@
-fn sort_by(arr: &mut [i32]) {}
-
 fn sort<T: Ord>(arr: &mut [T]) {
+    sort_by(arr, |a, b| a.lt(b))
+}
+
+fn sort_by<T: Ord, F: Fn(&T, &T) -> bool>(arr: &mut [T], p: F) {
     for x in (0..=(arr.len() - 1) / 2).rev() {
-        sink(arr, x, (arr.len() - 1));
+        sink(arr, x, (arr.len() - 1), &p);
     }
 
     let k = 0;
@@ -10,15 +12,15 @@ fn sort<T: Ord>(arr: &mut [T]) {
     while l > 0 {
         arr.swap(k, l);
         l = l - 1;
-        sink(arr, 0, l);
+        sink(arr, 0, l, &p);
     }
 }
 
-pub fn swim<T>(arr: &mut [T], k: usize) {
+pub fn swim<T: Ord>(arr: &mut [T], k: usize) {
     let mut k = k;
     while k > 0 {
         let parent = (k - 1) / 2;
-        if arr[parent] < arr[k] {
+        if arr[parent].lt(&arr[k]) {
             arr.swap(parent, k);
             k = parent;
         } else {
@@ -27,14 +29,14 @@ pub fn swim<T>(arr: &mut [T], k: usize) {
     }
 }
 
-pub fn sink<T: Ord>(arr: &mut [T], k: usize, n: usize) {
+pub fn sink<T: Ord, F: Fn(&T, &T) -> bool>(arr: &mut [T], k: usize, n: usize, p: &F) {
     let mut k = k;
     while 2 * k + 1 <= n {
         let mut j = 2 * k + 1;
-        if j + 1 <= n && arr[j] < arr[j + 1] {
+        if j + 1 <= n && p(&arr[j], &arr[j + 1]) {
             j += 1;
         }
-        if arr[j] > arr[k] {
+        if !p(&arr[j], &arr[k]) {
             arr.swap(k, j);
             k = j;
         } else {
@@ -57,14 +59,14 @@ mod tests {
     #[test]
     fn sink_test_1() {
         let mut v = vec![1, 7, 4, 5, 4, 3, 2, 2];
-        sink(&mut v, 0, 7);
+        sink(&mut v, 0, 7, &|a, b| a.lt(b));
         assert_eq!(v, vec![7, 5, 4, 2, 4, 3, 2, 1]);
     }
 
     #[test]
     fn sink_test_2() {
         let mut v = vec![1, 3, 5, 2, 1, 4, 3];
-        sink(&mut v, 0, 6);
+        sink(&mut v, 0, 6, &|a, b| a.lt(b));
         assert_eq!(v, vec![5, 3, 4, 2, 1, 1, 3]);
     }
 
@@ -81,5 +83,13 @@ mod tests {
         v.reverse();
         sort(&mut v);
         assert_eq!(v, vec![1, 2, 3, 4, 5, 6, 7])
+    }
+
+    #[test]
+    fn sort_test_3() {
+        let mut v = vec![1, 2, 3, 2, 3, 6, 6];
+        v.reverse();
+        sort(&mut v);
+        assert_eq!(v, vec![1, 2, 2, 3, 3, 6, 6])
     }
 }
