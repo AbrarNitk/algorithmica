a7e61cb71957a78930bd836d4a6208f05f791c7d
Abrar <abrar.nitk@gmail.com>
2021-06-18 10:55:45 +0530

    swim fixes

--- algorithmica/src/pq/max_pq.rs
+++ algorithmica/src/pq/max_pq.rs
@@ -17,21 +17,22 @@ impl MaxPQ {
         }
     }
 
-    pub fn from_arr(v: Vec<i32>)  -> Self {
+    pub fn from_arr(v: Vec<i32>) -> Self {
         let n = v.len();
-        let mut q = Vec::with_capacity(n+ 1);
+        let mut q = Vec::with_capacity(n + 1);
+        q.resize_with(n + 1, || 0);
         for i in 0..n {
-            q[i + 1] = v[i];
+            q.insert(i + 1, v[i]);
         }
         let mut max_pq = Self { q, n };
-        for i in (1..=n/2).rev() {
+        for i in (1..=n / 2).rev() {
             max_pq.sink(i);
         }
         max_pq
     }
 
     pub fn del_max(&mut self) -> i32 {
-        let max= self.q[1];
+        let max = self.q[1];
         self.exchange(1, self.n);
         self.n -= 1;
         self.sink(1);
@@ -59,7 +60,8 @@ impl MaxPQ {
     }
 
     fn swim(&mut self, mut k: usize) {
-        while k > 1 && self.less(k / 2, 2) {
+
+        while k > 1 && self.less(k / 2, k) {
             self.exchange(k / 2, k);
             k /= 2;
         }
@@ -75,7 +77,7 @@ impl MaxPQ {
 
     pub fn max(&self) -> i32 {
         // if is_empty throw exception
-        self.q[0]
+        self.q[1]
     }
 
     fn less(&self, i: usize, j: usize) -> bool {
@@ -84,5 +86,23 @@ impl MaxPQ {
 
     fn exchange(&mut self, i: usize, j: usize) {
         self.q.swap(i, j)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_max() {
+        let mut pq = MaxPQ::from_arr(vec![1, 2, 3, 4, 5, 6, 7]);
+        assert_eq!(pq.max(), 7);
+        pq.del_max();
+        assert_eq!(pq.max(), 6);
+        pq.del_max();
+        assert_eq!(pq.max(), 5);
+        pq.insert(15);
+        println!("{:?}", pq.q);
+        assert_eq!(pq.max(), 15);
     }
 }
