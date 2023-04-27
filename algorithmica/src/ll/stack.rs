92726a33f2eea7898e32d3cc5ee74a49bb49ddc8
AbrarNitk <abrar.nitk@gmail.com>
2023-04-27 21:17:23 +0530

    stack apis

--- algorithmica/src/ll/stack.rs
+++ algorithmica/src/ll/stack.rs
@@ -1,4 +1,5 @@
-struct Node<T> {
+
+pub struct Node<T> {
     data: T,
     next: Link<T>,
 }
@@ -13,4 +14,28 @@ impl<T> Stack<T> {
     pub fn new() -> Self {
         Self { head: None }
     }
+
+    pub fn prepend(&self, data: T) -> Stack<T> {
+        Stack {
+            head: Some(std::rc::Rc::new(Node {
+                data,
+                next: self.head.clone(),
+            })),
+        }
+    }
+
+    pub fn tail(&self) -> Stack<T> {
+        Stack {
+            head: self.head.as_ref().and_then(|x| x.next.clone()),
+        }
+    }
+
+    pub fn peek(&self) -> Option<&T> {
+        self.head.as_ref().map(|x| &x.data)
+    }
+
+    pub fn head(&self) -> Option<&Node<T>> {
+        self.head.as_deref()
+    }
+
 }
