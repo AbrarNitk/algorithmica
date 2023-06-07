615459285451f04401d74dd20843a3026e144b37
AbrarNitk <abrar.nitk@gmail.com>
2023-06-07 08:44:56 +0530

    dll: push and pop back tested

--- algorithmica/src/ll/dll.rs
+++ algorithmica/src/ll/dll.rs
@@ -11,13 +11,6 @@ pub struct LinkList<Item> {
     len: usize,
 }
 
-pub struct Iter<'a, Item> {
-    head: Link<Item>,
-    tail: Link<Item>,
-    len: usize,
-    _foo: std::marker::PhantomData<&'a Item>,
-}
-
 impl<Item> Node<Item> {
     fn new(value: Item) -> Box<Self> {
         Box::new(Node {
@@ -28,6 +21,13 @@ impl<Item> Node<Item> {
     }
 }
 
+pub struct Iter<'a, Item> {
+    head: Link<Item>,
+    tail: Link<Item>,
+    len: usize,
+    _foo: std::marker::PhantomData<&'a Item>,
+}
+
 impl<Item> LinkList<Item> {
     pub fn new() -> Self {
         Default::default()
@@ -36,11 +36,11 @@ impl<Item> LinkList<Item> {
     pub fn push_front(&mut self, value: Item) {
         // SAFETY: Box never return null pointer
         let new_node = unsafe { std::ptr::NonNull::new_unchecked(Box::into_raw(Node::new(value))) };
-        match self.head.take() {
+        match self.head {
             // SAFETY: head will never point to null pointer
             Some(old_node) => unsafe {
-                (*new_node.as_ptr()).next = Some(old_node);
                 (*old_node.as_ptr()).prev = Some(new_node);
+                (*new_node.as_ptr()).next = Some(old_node);
             },
             None => {
                 self.tail = Some(new_node);
@@ -53,7 +53,7 @@ impl<Item> LinkList<Item> {
     pub fn push_back(&mut self, value: Item) {
         // SAFETY: Box never return null pointer
         let new_node = unsafe { std::ptr::NonNull::new_unchecked(Box::into_raw(Node::new(value))) };
-        match self.tail.take() {
+        match self.tail {
             // SAFETY: tail will never point to null pointer
             Some(old_node) => unsafe {
                 (*old_node.as_ptr()).next = Some(new_node);
@@ -69,11 +69,13 @@ impl<Item> LinkList<Item> {
 
     pub fn pop_front(&mut self) -> Option<Item> {
         self.head.map(|current_head| unsafe {
+            // SAFETY: We are unboxing a valid pointer and only we are accessing it
+            // So it is safe to drop this pointer
             let boxed_node = Box::from_raw(current_head.as_ptr());
-            match (*current_head.as_ptr()).next {
+            self.head = boxed_node.next;
+            match self.head {
                 Some(new_head) => {
                     (*new_head.as_ptr()).prev = None;
-                    self.head = Some(new_head);
                 }
                 None => {
                     self.tail = None;
@@ -86,11 +88,13 @@ impl<Item> LinkList<Item> {
 
     pub fn pop_back(&mut self) -> Option<Item> {
         self.tail.map(|current_tail| unsafe {
+            // SAFETY: We are unboxing a valid pointer and only we are accessing it
+            // So it is safe to drop this pointer
             let boxed_node = Box::from_raw(current_tail.as_ptr());
-            match (*current_tail.as_ptr()).prev {
+            self.tail = boxed_node.prev;
+            match self.tail {
                 Some(new_tail) => {
                     (*new_tail.as_ptr()).next = None;
-                    self.tail = Some(new_tail);
                 }
                 None => {
                     self.head = None;
@@ -165,6 +169,28 @@ impl<Item: Clone> Clone for LinkList<Item> {
     }
 }
 
+// impl<Item> Extend<Item> for LinkList<Item> {
+//     fn extend<T: IntoIterator<Item = Item>>(&mut self, iter: T) {
+//         for item in iter {
+//             self.push_back(item);
+//         }
+//     }
+// }
+
+// impl<Item> std::iter::FromIterator<Item> for LinkList<Item> {
+//     fn from_iter<T: IntoIterator<Item = Item>>(iter: T) -> Self {
+//         let mut dll = LinkList::new();
+//         dll.extend(iter);
+//         dll
+//     }
+// }
+
+impl<Item: std::fmt::Debug> std::fmt::Debug for LinkList<Item> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_list().entries(self).finish()
+    }
+}
+
 impl<'a, Item> Iterator for Iter<'a, Item> {
     type Item = &'a Item;
 
@@ -194,14 +220,45 @@ impl<'a, Item> IntoIterator for &'a LinkList<Item> {
 mod test {
 
     #[test]
-    fn push_front() {}
+    fn push_front() {
+        let mut dll = super::LinkList::new();
+        dll.push_front("first-element");
+        dll.push_front("second-element");
+        dll.push_front("third-element");
+        // println!("{:?}", dll);
+        assert_eq!(dll.len(), 3);
+    }
 
     #[test]
-    fn push_back() {}
+    fn push_back() {
+        let mut dll = super::LinkList::new();
+        dll.push_back("first-element");
+        dll.push_back("second-element");
+        dll.push_back("third-element");
+        assert_eq!(dll.len(), 3);
+    }
 
     #[test]
-    fn pop_front() {}
+    fn pop_front() {
+        let mut dll = super::LinkList::new();
+        dll.push_back("first-element");
+        dll.push_back("second-element");
+        dll.push_back("third-element");
+        assert_eq!(dll.pop_front(), Some("first-element"));
+        assert_eq!(dll.pop_front(), Some("second-element"));
+        assert_eq!(dll.pop_front(), Some("third-element"));
+        assert_eq!(dll.pop_front(), None);
+    }
 
     #[test]
-    fn pop_back() {}
+    fn pop_back() {
+        let mut dll = super::LinkList::new();
+        dll.push_front("first-element");
+        dll.push_front("second-element");
+        dll.push_front("third-element");
+        assert_eq!(dll.pop_back(), Some("first-element"));
+        assert_eq!(dll.pop_back(), Some("second-element"));
+        assert_eq!(dll.pop_back(), Some("third-element"));
+        assert_eq!(dll.pop_back(), None);
+    }
 }
