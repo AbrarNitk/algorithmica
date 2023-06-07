5d3478d5f313b4171e79fd5a0bdda59cbc4bc73e
AbrarNitk <abrar.nitk@gmail.com>
2023-06-07 07:45:57 +0530

    dll: other fns

--- algorithmica/src/ll/dll.rs
+++ algorithmica/src/ll/dll.rs
@@ -67,9 +67,9 @@ impl<Item> LinkList<Item> {
     }
 
     pub fn pop_front(&mut self) -> Option<Item> {
-        self.head.map(|x| unsafe {
-            let boxed_node = Box::from_raw(x.as_ptr());
-            match (*x.as_ptr()).next {
+        self.head.map(|current_head| unsafe {
+            let boxed_node = Box::from_raw(current_head.as_ptr());
+            match (*current_head.as_ptr()).next {
                 Some(new_head) => {
                     (*new_head.as_ptr()).prev = None;
                     self.head = Some(new_head);
@@ -81,6 +81,51 @@ impl<Item> LinkList<Item> {
             self.len -= 1;
             boxed_node.value
         })
+    }
+
+    pub fn pop_back(&mut self) -> Option<Item> {
+        self.tail.map(|current_tail| unsafe {
+            let boxed_node = Box::from_raw(current_tail.as_ptr());
+            match (*current_tail.as_ptr()).prev {
+                Some(new_tail) => {
+                    (*new_tail.as_ptr()).next = None;
+                    self.tail = Some(new_tail);
+                }
+                None => {
+                    self.head = None;
+                }
+            }
+            self.len -= 1;
+            boxed_node.value
+        })
+    }
+
+    pub fn front(&self) -> Option<&Item> {
+        self.head.as_ref().map(|x| unsafe { &(*x.as_ptr()).value })
+    }
+
+    pub fn back(&self) -> Option<&Item> {
+        self.tail.map(|x| unsafe { &(*x.as_ptr()).value })
+    }
+
+    pub fn front_mut(&mut self) -> Option<&mut Item> {
+        self.head.map(|x| unsafe { &mut (*x.as_ptr()).value })
+    }
+
+    pub fn back_mut(&mut self) -> Option<&mut Item> {
+        self.tail.map(|x| unsafe { &mut (*x.as_ptr()).value })
+    }
+
+    pub fn clear(&mut self) {
+        while let Some(_) = self.pop_back() {}
+    }
+
+    pub fn len(&self) -> usize {
+        self.len
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.len() == 0
     }
 }
 
