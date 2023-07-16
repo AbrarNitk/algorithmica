0b5813152524452179af9f38630a201f7cd6d379
AbrarNitk <abrar.nitk@gmail.com>
2023-07-16 22:44:37 +0530

    reading file from fs

--- sample-projects/md-renderer/src/handler.rs
+++ sample-projects/md-renderer/src/handler.rs
@@ -1,9 +1,15 @@
+pub async fn test() -> Result<String, ()> {
+    let content = crate::utils::read_file(std::path::Path::new("test.md"))
+        .await
+        .expect("can not read the file path");
+    Ok(content)
+}
 pub async fn handle(req: hyper::Request<hyper::Body>) -> Result<hyper::Response<hyper::Body>, ()> {
     match (req.method(), req.uri().path()) {
         (&hyper::Method::GET, "/") => {
             let mut response = hyper::Response::new(hyper::Body::empty());
             // let resp = http_service::controller::get_user_profile()?;
-            // *response.body_mut() = hyper::Body::from(serde_json::to_string(&resp)?);
+            *response.body_mut() = hyper::Body::from(test().await?);
             *response.status_mut() = hyper::StatusCode::OK;
             response.headers_mut().append(
                 hyper::header::CONTENT_TYPE,
---
+++ sample-projects/test.md
@@ -0,0 +1,8 @@
+# Test File
+
+## Heading Second Level
+
+- list item 1
+- list item 2
+- list item 3
+- list item 4
